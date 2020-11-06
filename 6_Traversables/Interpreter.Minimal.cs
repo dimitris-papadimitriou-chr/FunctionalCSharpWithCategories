2a1bac4b8a7f2d7eaeb1db16b68c918d02124183
dimit <dimit@DESKTOP-T6V9AM2>
2020-11-06 23:01:31 +0200

    Create Interpreter.Minimal.cs

---
+++ 6_Traversables/Interpreter.Minimal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traversables.Interpreter.Minimal
+{
+
+
+    public class Demo
+    {
+        public static void Run()
+        {
+            {
+                Func<string, Func<Dictionary<string, int>, int>> Var = (variable) => (context) => context[variable];
+
+                Func<Func<Dictionary<string, int>, int>,
+                    Func<Dictionary<string, int>, int>,
+                    Func<Dictionary<string, int>, int>>
+                  Add = (l, r) => (context) => l(context) + r(context);
+
+                Func<Func<Dictionary<string, int>, int>,
+                    Func<Dictionary<string, int>, int>,
+                    Func<Dictionary<string, int>, int>>
+                  Sub = (l, r) => (context) => l(context) + r(context);
+
+                Func<Dictionary<string, int>, int> expr = Add(Add(Var("a"), Var("b")), Var("b"));
+            }
+
+            {
+                Func<int, Func<int, int>> Var = (variable) => (context) => variable;
+
+                Func<Func<int, int>,
+               Func<int, int>,
+               Func<int, int>>
+                   Add = (l, r) => (context) => l(context) + r(context);
+
+                Func<Func<int, int>,
+                Func<int, int>,
+                Func<int, int>>
+                    Sub = (l, r) => (context) => l(context) + r(context);
+
+                Func<int, int> expr = Add(Add(Var(2), Var(3)), Var(4));
+            }
+        }
+    }
+
+
+
+}
